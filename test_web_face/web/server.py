fa1b3441a28ac3a1a7a9ea89e2b3f7186246bbbe
Joaquin Ramirez <juacsr@gmail.com>
2020-10-25 14:41:16 -0500

    testing on the web

---
+++ test_web_face/web/server.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template, request, session, Response, redirect
+from database import connector
+from model import entities
+import json
+import time
+import functions
+
+db = connector.Manager()
+engine = db.createEngine()
+
+app = Flask(__name__)
+
+
+@app.route('/static/<content>')
+def static_content(content):
+    return render_template(content)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+    msg = json.loads(request.data)
+    #dummy validation
+    if msg['username'] == msg['password'] :
+        r_msg = {'msg':'Welcome'}
+        json_msg = json.dumps(r_msg)
+        return Response(json_msg, status=200);
+    r_msg = {'msg':'Failed'}
+    json_msg = json.dumps(r_msg)
+    return Response(json_msg, status=401)
+
+@app.route('/test', methods=['POST'])
+def test():
+    functions.runTest()
+    r_msg = {'msg':'Finished'}
+    json_msg = json.dumps(r_msg)
+    return Response(json_msg, status=401)
+
+
+if __name__ == '__main__':
+    app.secret_key = ".."
+    app.run(port=8080, threaded=True, host=('127.0.0.1'))
---
+++ test_web_face/web/static/js/login.js
@@ -0,0 +1,37 @@
+function login(){
+    console.log("Login User");
+    var username = $('#username').val();  //getting username by ID
+    var password = $('#password').val();  // getting password by id
+
+    var credentials = {'username':username, 'password':password};
+    $.post({
+        url:'/authenticate',
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function(data){
+            console.log("Authenticated!");
+            alert(data['msg']);
+
+        },
+        data: JSON.stringify(credentials)
+    });
+}
+
+function runTest(){
+    console.log("Running Test");
+    //var credentials = {'username':username, 'password':password};
+    $.ajax({
+        url:'/test',
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function(data){
+            console.log("Finished test!");
+            alert(data['msg']);
+
+        },
+        //data: JSON.stringify(credentials)
+    });
+}
+
